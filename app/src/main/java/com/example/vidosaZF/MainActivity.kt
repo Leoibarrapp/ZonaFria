85b16801b4b2623977da948017bc4203c733360f
Leonardo Ibarra <laibarra.23@est.ucab.edu.ve>
2025-02-27 18:37:21 -0400

    Observadores para uds y eficiencia
    Cálculo de las unidades totales de cada hora y las unidades totales del turno, listos
    
    Cálculo de la eficiencia de cada hora y la eficiencia total, listos
    
    Parte del servidor por arreglar

--- app/src/main/java/com/example/vidosaZF/MainActivity.kt
+++ app/src/main/java/com/example/vidosaZF/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.vidosaZF
 
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import com.android.volley.toolbox.JsonObjectRequest
 import org.json.JSONArray
 import org.json.JSONObject
@@ -19,6 +23,7 @@ import java.util.Locale
 import com.android.volley.Request
 import com.android.volley.Response
 import org.json.JSONException
+import java.text.DecimalFormat
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,14 +33,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var horas2TVs: List<TextView>
     private lateinit var fechaTV: TextView
     private lateinit var titulo: TextView
+    private lateinit var udsHoraET: EditText
     private lateinit var udsTotalesTurnoTV: TextView
-    private lateinit var udsTotalesHoraETs: List<EditText>
-    private lateinit var celdasTabla1: List<EditText>
+    private lateinit var udsTotalesHoraTVs: List<TextView>
+    private lateinit var eficienciaHoraTVs: List<TextView>
+    private lateinit var eficienciaTotalTurno: TextView
+    private lateinit var celdasTabla1: List<List<EditText>>
     private lateinit var btnGuardar: Button
+
+    private lateinit var tabla: TableLayout
+
     private val serverUrl = "http://192.168.68.64:8080"
     private lateinit var queue: com.android.volley.RequestQueue
-
-
 
     private var turno: Int = 0
 
@@ -58,107 +67,117 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabla = findViewById(R.id.table)
+
         btnGuardar= findViewById(R.id.btn_submit)
         titulo = findViewById(R.id.tv_title)
         fechaTV = findViewById(R.id.tv_fecha)
+        udsHoraET = findViewById(R.id.et_uds_hora)
+
         udsTotalesTurnoTV = findViewById(R.id.tv_uds_totales_turno)
-        val list2= listOf(R.id.A1_Hrs, R.id.A2_Hrs, R.id.A3_Hrs, R.id.A4_Hrs, R.id.A5_Hrs, R.id.A6_Hrs, R.id.A7_Hrs, R.id.A8_Hrs,R.id.A9_Def1,R.id.A10_Def2,R.id.A11_Def3,R.id.A12_Def4,R.id.A13_Def5,R.id.A14_Def6,R.id.B1_Hrs,R.id.B2_Hrs,R.id.B3_Hrs,R.id.B4_Hrs,R.id.B5_Hrs,R.id.B6_Hrs,R.id.B7_Hrs,R.id.B8_Hrs,R.id.B9_Def1,R.id.B10_Def2,R.id.B11_Def3,R.id.B12_Def4,R.id.B13_Def5,R.id.B14_Def6,R.id.C1_Hrs,R.id.C2_Hrs,R.id.C3_Hrs,R.id.C4_Hrs,R.id.C5_Hrs,R.id.C6_Hrs,R.id.C7_Hrs,R.id.C8_Hrs,R.id.C9_Def1,R.id.C10_Def2,R.id.C11_Def3,R.id.C12_Def4,R.id.C13_Def5,R.id.C14_Def6,R.id.D1_Hrs,R.id.D2_Hrs,R.id.D3_Hrs,R.id.D4_Hrs,R.id.D5_Hrs,R.id.D6_Hrs,R.id.D7_Hrs,R.id.D8_Hrs,R.id.D9_Def1,R.id.D10_Def2,R.id.D11_Def3,R.id.D12_Def4,R.id.D13_Def5,R.id.D14_Def6,R.id.E1_Hrs,R.id.E2_Hrs,R.id.E3_Hrs,R.id.E4_Hrs,R.id.E5_Hrs,R.id.E6_Hrs,R.id.E7_Hrs,R.id.E8_Hrs,R.id.E9_Def1,R.id.E10_Def2,R.id.E11_Def3,R.id.E12_Def4,R.id.E13_Def5,R.id.E14_Def6,R.id.F1_Hrs,R.id.F2_Hrs,R.id.F3_Hrs,R.id.F4_Hrs,R.id.F5_Hrs,R.id.F6_Hrs,R.id.F7_Hrs,R.id.F8_Hrs,R.id.F9_Def1,R.id.F10_Def2,R.id.F11_Def3,R.id.F12_Def4,R.id.F13_Def5,R.id.F14_Def6,R.id.H1_Hrs,R.id.H2_Hrs,R.id.H3_Hrs,R.id.H4_Hrs,R.id.H5_Hrs,R.id.H6_Hrs,R.id.H7_Hrs,R.id.H8_Hrs,R.id.H9_Def1,R.id.H10_Def2,R.id.H11_Def3,R.id.H12_Def4,R.id.H13_Def5,R.id.H14_Def6,R.id.I1_Hrs,R.id.I2_Hrs,R.id.I3_Hrs,R.id.I4_Hrs,R.id.I5_Hrs,R.id.I6_Hrs,R.id.I7_Hrs,R.id.I8_Hrs,R.id.I9_Def1,R.id.I10_Def2,R.id.I11_Def3,R.id.I12_Def4,R.id.I13_Def5,R.id.I14_Def6)
-        val list = listOf(R.id.et_uds_1, R.id.et_uds_2, R.id.et_uds_3, R.id.et_uds_4, R.id.et_uds_5, R.id.et_uds_6, R.id.et_uds_7, R.id.et_uds_8)
-        celdasTabla1 = list2.map { id -> findViewById(id) }
-        udsTotalesHoraETs = list.map { id -> findViewById(id) }
+        eficienciaTotalTurno = findViewById(R.id.tv_eficiencia_total_turno)
+
+        horas1TVs = listOf(R.id.tv_hora1_tabla1, R.id.tv_hora2_tabla1, R.id.tv_hora3_tabla1, R.id.tv_hora4_tabla1, R.id.tv_hora5_tabla1, R.id.tv_hora6_tabla1, R.id.tv_hora7_tabla1, R.id.tv_hora8_tabla1)
+            .map { id -> findViewById(id) }
+        horas2TVs = listOf(R.id.tv_hora1_tabla2, R.id.tv_hora2_tabla2, R.id.tv_hora3_tabla2, R.id.tv_hora4_tabla2, R.id.tv_hora5_tabla2, R.id.tv_hora6_tabla2, R.id.tv_hora7_tabla2, R.id.tv_hora8_tabla2)
+            .map { id -> findViewById(id) }
+
+        //Lista de filas, que a su vez tienen la lista de celdas de cada fila
+        celdasTabla1 = listOf(
+            listOf(R.id.A1_Hrs,R.id.A2_Hrs,R.id.A3_Hrs,R.id.A4_Hrs,R.id.A5_Hrs,R.id.A6_Hrs,R.id.A7_Hrs,R.id.A8_Hrs,R.id.A9_Def1,R.id.A10_Def2,R.id.A11_Def3,R.id.A12_Def4,R.id.A13_Def5,R.id.A14_Def6),
+            listOf(R.id.B1_Hrs,R.id.B2_Hrs,R.id.B3_Hrs,R.id.B4_Hrs,R.id.B5_Hrs,R.id.B6_Hrs,R.id.B7_Hrs,R.id.B8_Hrs,R.id.B9_Def1,R.id.B10_Def2,R.id.B11_Def3,R.id.B12_Def4,R.id.B13_Def5,R.id.B14_Def6),
+            listOf(R.id.C1_Hrs,R.id.C2_Hrs,R.id.C3_Hrs,R.id.C4_Hrs,R.id.C5_Hrs,R.id.C6_Hrs,R.id.C7_Hrs,R.id.C8_Hrs,R.id.C9_Def1,R.id.C10_Def2,R.id.C11_Def3,R.id.C12_Def4,R.id.C13_Def5,R.id.C14_Def6),
+            listOf(R.id.D1_Hrs,R.id.D2_Hrs,R.id.D3_Hrs,R.id.D4_Hrs,R.id.D5_Hrs,R.id.D6_Hrs,R.id.D7_Hrs,R.id.D8_Hrs,R.id.D9_Def1,R.id.D10_Def2,R.id.D11_Def3,R.id.D12_Def4,R.id.D13_Def5,R.id.D14_Def6),
+            listOf(R.id.E1_Hrs,R.id.E2_Hrs,R.id.E3_Hrs,R.id.E4_Hrs,R.id.E5_Hrs,R.id.E6_Hrs,R.id.E7_Hrs,R.id.E8_Hrs,R.id.E9_Def1,R.id.E10_Def2,R.id.E11_Def3,R.id.E12_Def4,R.id.E13_Def5,R.id.E14_Def6),
+            listOf(R.id.F1_Hrs,R.id.F2_Hrs,R.id.F3_Hrs,R.id.F4_Hrs,R.id.F5_Hrs,R.id.F6_Hrs,R.id.F7_Hrs,R.id.F8_Hrs,R.id.F9_Def1,R.id.F10_Def2,R.id.F11_Def3,R.id.F12_Def4,R.id.F13_Def5,R.id.F14_Def6),
+            listOf(R.id.G1_Hrs,R.id.G2_Hrs,R.id.G3_Hrs,R.id.G4_Hrs,R.id.G5_Hrs,R.id.G6_Hrs,R.id.G7_Hrs,R.id.G8_Hrs,R.id.G9_Def1,R.id.G10_Def2,R.id.G11_Def3,R.id.G12_Def4,R.id.G13_Def5,R.id.G14_Def6),
+            listOf(R.id.H1_Hrs,R.id.H2_Hrs,R.id.H3_Hrs,R.id.H4_Hrs,R.id.H5_Hrs,R.id.H6_Hrs,R.id.H7_Hrs,R.id.H8_Hrs,R.id.H9_Def1,R.id.H10_Def2,R.id.H11_Def3,R.id.H12_Def4,R.id.H13_Def5,R.id.H14_Def6)
+        ).map { fila ->
+            fila.map { id ->
+                val celda = findViewById<EditText>(id)
+                celda.tag = resources.getResourceEntryName(id)
+                    observarCambiosCeldas(celda)
+                celda
+            }
+        }
+
+        // Inicializa el Map con los EditText correspondientes una vez, para acceder hacer:      udsTotalesHoraETs['X']
+        udsTotalesHoraTVs = listOf(R.id.tv_uds_A, R.id.tv_uds_B, R.id.tv_uds_C, R.id.tv_uds_D, R.id.tv_uds_E, R.id.tv_uds_F, R.id.tv_uds_G, R.id.tv_uds_H)
+            .map { id ->
+                val udsTotalesHora = findViewById<TextView>(id)
+                udsTotalesHora.tag = resources.getResourceEntryName(id)
+                    observarCambiosUdsTotalesHora(udsTotalesHora)
+                udsTotalesHora
+            }
+
+        eficienciaHoraTVs = listOf(R.id.tv_eficiencia_A, R.id.tv_eficiencia_B, R.id.tv_eficiencia_C, R.id.tv_eficiencia_D, R.id.tv_eficiencia_E, R.id.tv_eficiencia_F, R.id.tv_eficiencia_G, R.id.tv_eficiencia_H)
+            .map { id ->
+                val eficienciaHora = findViewById<TextView>(id)
+                eficienciaHora.tag = resources.getResourceEntryName(id)
+                    observarCambiosEficienciaHora(eficienciaHora)
+                eficienciaHora
+            }
+
         queue = Volley.newRequestQueue(this) // Initialize the request queue here
-
-
-        // Obtiene los TextViews de las horas para actualizarlos posteriormente
-
-        obtenerHorasTextViews()
-        calcularUdsTotalesTurno()
 
         // Inicia la ejecución del Runnable
         handler.post(runnableCode)
-        btnGuardar.setOnClickListener{
-            enviarDatosTablaUsuario()
-        }
+
+//        btnGuardar.setOnClickListener{
+//            enviarDatosTablaUsuario()
+//        }
     }
 
 
 
 
-        fun enviarDatosTablaUsuario() {
-                val casillasTabla1=celdasTabla1.map { et ->
-                    val text = et.text.toString()
-                    if (text.isNotEmpty() && text.matches(Regex("\\d+"))) {
-                        text.toInt()
-                    } else {
-                        0 // O un valor por defecto que prefieras
-                }}
-                val udsPorHora = udsTotalesHoraETs.map { et ->
-                    val text = et.text.toString()
-                    if (text.isNotEmpty() && text.matches(Regex("\\d+"))) {
-                        text.toInt()
-                    } else {
-                        0 // O un valor por defecto que prefieras
-                    }
-                }
+
+//    fun enviarDatosTablaUsuario() {
+//        val casillasTabla1=celdasTabla1.map { et ->
+//            val text = et.text.toString()
+//            if (text.isNotEmpty() && text.matches(Regex("\\d+"))) {
+//                text.toInt()
+//            } else {
+//                0 // O un valor por defecto que prefieras
+//        }}
+//        val udsPorHora = udsTotalesHoraTVs.map { tv ->
+//            val text = tv.text.toString()
+//            if (text.isNotEmpty() && text.matches(Regex("\\d+"))) {
+//                text.toInt()
+//            } else {
+//                0 // O un valor por defecto que prefieras
+//            }
+//        }
+//
+//        try {
+//            val jsonObject = JSONObject()
+//            jsonObject.put("celdas", JSONArray(casillasTabla1))
+//            jsonObject.put("udsPorHora", JSONArray(udsPorHora))
+//            val url = "${serverUrl}/enviar_datos_usuario" // URL para la solicitud POST
+//            val jsonObjectRequest = JsonObjectRequest(
+//                Request.Method.POST, url, jsonObject,
+//                { response ->
+//                    // Manejar la respuesta del servidor
+//                    titulo.text = "Datos enviados correctamente"
+//                },
+//                { error ->
+//                    // Manejar el error
+//                    titulo.text = "Error al enviar los datos: ${error.message}"
+//                })
+//
+//            queue.add(jsonObjectRequest)
+//
+//        } catch (e: JSONException) {
+//            titulo.text = "Error al crear JSON: ${e.message}"
+//            e.printStackTrace()
+//        }
+//        // 3. Enviar los datos al servidor
+//    }
 
 
 
 
-
-                
-
-            
-            
-            try {
-                val jsonObject = JSONObject()
-                jsonObject.put("celdas", JSONArray(casillasTabla1))
-                jsonObject.put("udsPorHora", JSONArray(udsPorHora))
-                val url = "${serverUrl}/enviar_datos_usuario" // URL para la solicitud POST
-                val jsonObjectRequest = JsonObjectRequest(
-                    Request.Method.POST, url, jsonObject,
-                    Response.Listener { response ->
-                        // Manejar la respuesta del servidor
-                        titulo.text = "Datos enviados correctamente"
-                    },
-                    Response.ErrorListener { error ->
-                        // Manejar el error
-                        titulo.text = "Error al enviar los datos: ${error.message}"
-                    })
-
-                queue.add(jsonObjectRequest)
-
-            } catch (e: JSONException) {
-                titulo.text = "Error al crear JSON: ${e.message}"
-                e.printStackTrace()
-            }
-            // 3. Enviar los datos al servidor
-
-
-
-
-
-        }
-
-
-    fun guardarDatos(){
-
-    }
-
-    // Obtiene todos los textViews de las horas
-    fun obtenerHorasTextViews() {
-        val horasIds1 = listOf(
-            R.id.tv_hora1_tabla1, R.id.tv_hora2_tabla1, R.id.tv_hora3_tabla1, R.id.tv_hora4_tabla1,
-            R.id.tv_hora5_tabla1, R.id.tv_hora6_tabla1, R.id.tv_hora7_tabla1, R.id.tv_hora8_tabla1
-        )
-        val horasIds2 = listOf(
-            R.id.tv_hora1_tabla2, R.id.tv_hora2_tabla2, R.id.tv_hora3_tabla2, R.id.tv_hora4_tabla2,
-            R.id.tv_hora5_tabla2, R.id.tv_hora6_tabla2, R.id.tv_hora7_tabla2, R.id.tv_hora8_tabla2
-        )
-
-        // Inicializa los TextViews para mostrar las horas en turno 1 y turno 2
-        horas1TVs = horasIds1.map { id -> findViewById(id) }
-        horas2TVs = horasIds2.map { id -> findViewById(id) }
-    }
 
     // Obtiene la hora actual
     fun obtenerHora(): Int {
@@ -172,21 +191,21 @@ class MainActivity : AppCompatActivity() {
     fun decidirTurno(): Int {
         val hora = obtenerHora()
 
-        return when {
-            hora in 7..14 -> 1
-            hora in 15..22 -> 2
-            hora == 23 || hora in 0..6 -> 3
+        return when (hora) {
+            in 7..14 -> 1
+            in 15..22 -> 2
+            23, in 0..6 -> 3
             else -> throw IllegalArgumentException("Hora inválida: $hora")
         }
     }
 
-    // Actualiza los turnos y los TextViews correspondientes
+    // Actualiza los turnos y los TextViews correspondientes al cambiar de hora
     fun actualizarTurno() {
         val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
         val currentDate = dateFormat.format(Calendar.getInstance().time) // Usamos .time para obtener la fecha completa
-        fechaTV.setText(currentDate)
+        fechaTV.text = currentDate
 
-            turno = decidirTurno()
+        turno = decidirTurno()
 
         val horasTurno = when (turno) {
             1 -> listOf("7:00 am", "8:00 am", "9:00 am", "10:00 am", "11:00 am", "12:00 pm", "1:00 pm", "2:00 pm")
@@ -202,29 +221,144 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun calcularUdsTotalesTurno() {
-        udsTotalesHoraETs.forEach { et ->
-            et.addTextChangedListener(object : TextWatcher {
+
+
+
+
+    //Le aplica el listener de texto a las celdas para actualizar los totales de cada hora
+    fun observarCambiosCeldas(celda: EditText) {
+        if(!celda.tag.toString().contains("Def")) {
+            celda.addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun afterTextChanged(s: Editable?) {}
 
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    sumarUds()
+                    val fila: Char = celda.tag.toString().first()
+                    calcularUdsTotalesHora(fila)
                 }
-                override fun afterTextChanged(s: Editable?) {}
             })
         }
     }
 
-    fun sumarUds() {
-        var sum = 0
-        udsTotalesHoraETs.forEach{ et ->
-            val text = et.text.toString()
+    fun observarCambiosUdsTotalesHora(tv: TextView) {
+        tv.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
 
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val fila = tv.tag.toString().last()
+                calcularEficienciaHora(fila)
+                calcularUdsTotalesTurno()
+            }
+        })
+    }
+
+    fun observarCambiosEficienciaHora(tv: TextView) {
+        tv.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                calcularEficienciaTotalTurno()
+            }
+        })
+    }
+
+    fun calcularUdsTotalesHora(fila: Char) {
+        var suma = 0
+        val posicionFila = obtenerPosicionDeFila(fila)
+
+        celdasTabla1[posicionFila].forEach { celdaET ->
+            suma += celdaET.text.toString().toIntOrNull() ?: 0
+        }
+
+        if(suma != 0) {
+            udsTotalesHoraTVs[posicionFila].text = suma.toString()
+            calcularEficienciaHora(fila)
+        }
+        else {
+            udsTotalesHoraTVs[posicionFila].text = ""
+        }
+    }
+
+    fun calcularUdsTotalesTurno() {
+        var suma = 0
+
+        udsTotalesHoraTVs.forEach { tv ->
+            val text = tv.text.toString()
             if (text.isNotEmpty()) {
-                sum += text.toInt()
+                suma += text.toInt()
             }
         }
 
-        udsTotalesTurnoTV.text = sum.toString()
+        if (suma != 0) {
+            udsTotalesTurnoTV.text = suma.toString()
+        } else {
+            udsTotalesTurnoTV.text = ""
+        }
+    }
+
+    //Calcula la eficiencia de cada hora en base a las Uds/Hora y el total de Unidades de esa hora específica
+    fun calcularEficienciaHora(fila: Char) {
+        val eficiencia: Double
+        val posicionFila = obtenerPosicionDeFila(fila)
+
+        val udsHoraGeneral = udsHoraET.text.toString().toDoubleOrNull()
+        val udsTotalesHora = udsTotalesHoraTVs[posicionFila].text.toString().toDoubleOrNull()
+
+        val eficienciaTV = eficienciaHoraTVs[posicionFila]
+
+        if (udsHoraGeneral != null && udsTotalesHora != null) {
+            eficiencia = (udsTotalesHora.times(100)) / udsHoraGeneral
+
+            when (eficiencia) {
+                in 0.0..50.0 -> eficienciaTV.setBackgroundResource(R.drawable.red_cell_shape)
+                in 50.0..80.0 -> eficienciaTV.setBackgroundResource(R.drawable.yellow_cell_shape)
+                in 80.0..100.0 -> eficienciaTV.setBackgroundResource(R.drawable.green_cell_shape)
+                else -> eficienciaTV.setBackgroundColor(Color.WHITE)
+            }
+
+            eficienciaTV.text = String.format(Locale.getDefault(),"%.2f", eficiencia)
+        }
+        else {
+            eficienciaTV.text = ""
+            eficienciaTV.setBackgroundResource(R.drawable.cell_shape)
+        }
+    }
+
+    fun calcularEficienciaTotalTurno() {
+        var suma = 0.0
+        var contadorValidos = 0
+
+        eficienciaHoraTVs.forEach { tv ->
+            val text = tv.text.toString()
+
+            if(text.isNotEmpty()) {
+                contadorValidos++
+                suma += text.toDouble()
+            }
+        }
+
+        if (contadorValidos > 0) {
+            val promedio = suma / contadorValidos
+            eficienciaTotalTurno.text = String.format(Locale.getDefault(),"%.2f%%", promedio)
+        }
+        else {
+            eficienciaTotalTurno.text = ""
+        }
+    }
+
+    fun obtenerPosicionDeFila(charFila: Char): Int {
+        return when(charFila) {
+            'A' -> 0
+            'B' -> 1
+            'C' -> 2
+            'D' -> 3
+            'E' -> 4
+            'F' -> 5
+            'G' -> 6
+            'H' -> 7
+            else -> throw IllegalArgumentException("Fila inválida: $charFila")
+        }
     }
 }
